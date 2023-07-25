13811b3d05aea90c69c3e5b9c8dc2c9295b6cfc2
OscarEscriba <128449179+OscarEscriba@users.noreply.github.com>
2023-07-25 12:32:03 -0600

    primera parte, clase variables.

---
+++ 01- Fundamentos/IntroVariablesYcomentarios.js
@@ -0,0 +1,19 @@
+//clase introduccion variables y comentarios.  
+//siempre inciar la variable con let y var. let es la manera mas actualizada de inicializar variables. 
+let a =10; 
+let b=10; 
+var c=10; 
+const c=10; //crea una constante, este 10 no va a poder cambiar.  
+
+//variables para la seguunda parte 
+let x =10; 
+let y =30; 
+let z =40; 
+let t = x +y; 
+console.log(t); 
+
+
+
+
+
+
